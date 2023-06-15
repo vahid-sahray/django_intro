be1113be3d32cfdddd95aa6c5bf2b163b3e04cd2
vahid <vahidsahray93@gmail.com>
2023-06-15 19:43:57 +0330

    reading models in template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Todo
 
 
 def home(request):
-    return render(request, 'home.html')
+    todo = Todo.objects.all()
+    return render(request, 'home.html', {'todo': todo})
