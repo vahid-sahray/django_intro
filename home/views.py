2e984b00d743b789d585c91d08494ed2c62f3ffd
vahid <vahidsahray93@gmail.com>
2023-06-15 18:27:04 +0330

    added templates dirand Introducing it to the program

--- home/views.py
+++ home/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Hello baby...')
-
+    return render(request, 'home.html')
