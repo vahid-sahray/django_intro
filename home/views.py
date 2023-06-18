e29b8f15b817f524b745cc94dd51f2a08a8169a2
vahid <vahidsahray93@gmail.com>
2023-06-18 06:44:29 +0330

    added  onetime messages

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Todo
+from django.contrib import messages
 
 
 def home(request):
@@ -14,4 +15,5 @@ def detail(request, post_id):
 
 def delete(request, post_id):
     todo = Todo.objects.filter(pk=post_id).delete()
+    messages.success(request, 'todo successfully', 'success')
     return redirect('home')
