0fc73f71dbc9278f657a810524a4db86f24bb0a0
vahid <vahidsahray93@gmail.com>
2023-06-18 07:46:18 +0330

    create model forms and added create-submit to home-page

--- home/views.py
+++ home/views.py
@@ -20,5 +20,13 @@ def delete(request, post_id):
 
 
 def created(request):
-    form = TodoCreatedForm()
+    if request.method == 'POST':
+        form = TodoCreatedForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            Todo.objects.create(title=cd['title'], body=cd['body'], created=cd['created'])
+            messages.success(request, 'Todo created successfully', 'success')
+            return redirect('home')
+    else:
+        form = TodoCreatedForm()
     return render(request, 'create.html', {'form': form})
