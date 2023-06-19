c7147d5011db9363c9f5c7f2c7eee356cedba382
vahid <vahidsahray93@gmail.com>
2023-06-19 07:33:56 +0330

    create register page to registration user

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def user_register(request):
-    pass
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'user registered successfully', 'success')
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
