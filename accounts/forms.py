0ab103541784fa72b0c6e4639ff8bc43d6fb7528
vahid <vahidsahray93@gmail.com>
2023-06-19 22:50:11 +0330

    create login func and page

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,3 +5,9 @@ class UserRegistrationForm(forms.Form):
     username = forms.CharField()
     email = forms.EmailField()
     password = forms.CharField()
+
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
